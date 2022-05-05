3fe1d58de237592afb0f528126766221b89f4dec
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-04 22:24:45 -0500

    Importar ng2-charts, y grafico barras

--- src/app/graphic/graphic.module.ts
+++ src/app/graphic/graphic.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgChartsModule } from 'ng2-charts';
+
 import { GraphicRoutingModule } from './graphic-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDoubleComponent } from './pages/barras-double/barras-double.component';
 import { DonutComponent } from './pages/donut/donut.component';
 import { DonutHttpComponent } from './pages/donut-http/donut-http.component';
 import { GraphicBarraComponent } from './components/graphic-barra/graphic-barra.component';
+
 
 
 @NgModule({
@@ -19,7 +22,8 @@ import { GraphicBarraComponent } from './components/graphic-barra/graphic-barra.
   ],
   imports: [
     CommonModule,
-    GraphicRoutingModule
+    GraphicRoutingModule,
+    NgChartsModule
   ]
 })
 export class GraphicModule { }
