c1a8bb82d26b466371713180cd08a5bc682541ca
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-04 21:49:00 -0500

    Estructura del proyecto

---
+++ src/app/graphic/components/graphic-barra/graphic-barra.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphic-barra',
+  templateUrl: './graphic-barra.component.html',
+  styles: [
+  ]
+})
+export class GraphicBarraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/graphic/graphic.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraphicRoutingModule } from './graphic-routing.module';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { BarrasDoubleComponent } from './pages/barras-double/barras-double.component';
+import { DonutComponent } from './pages/donut/donut.component';
+import { DonutHttpComponent } from './pages/donut-http/donut-http.component';
+import { GraphicBarraComponent } from './components/graphic-barra/graphic-barra.component';
+
+
+@NgModule({
+  declarations: [
+    BarrasComponent,
+    BarrasDoubleComponent,
+    DonutComponent,
+    DonutHttpComponent,
+    GraphicBarraComponent
+  ],
+  imports: [
+    CommonModule,
+    GraphicRoutingModule
+  ]
+})
+export class GraphicModule { }
