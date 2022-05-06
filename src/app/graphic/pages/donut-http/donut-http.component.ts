ac00538ba18596d88adec0dcdbe5235a1df47dfe
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-06 00:46:04 -0500

    dona grafica

--- src/app/graphic/pages/donut-http/donut-http.component.ts
+++ src/app/graphic/pages/donut-http/donut-http.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GraphicsService } from '../../services/graphics.service';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 @Component({
   selector: 'app-donut-http',
   templateUrl: './donut-http.component.html',
@@ -8,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonutHttpComponent implements OnInit {
 
-  constructor() { }
+  donutData:any;
+  dataLabels: string[]=[];
+  dataSets: number[]=[];
+
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: this.dataLabels,
+    datasets: [ {
+      data: this.dataSets
+    } ]
+  };
+  public pieChartType: ChartType = 'pie';
+  
+  constructor(private graphicsService:GraphicsService) { }
 
   ngOnInit(): void {
+    this.donutData = this.graphicsService.getUserSocialNetwork().subscribe(resp=>{
+      const ketData = Object.keys(resp);
+      const data = Object.values(resp);
+      ketData.forEach(e=>{this.dataLabels.push(e)});
+      data.forEach(e=>{this.dataSets.push(e)});
+      // console.log("la: "+this.dataLabels)
+      // console.log("Se: "+this.dataSets)
+    })
   }
 
 }
--- src/app/graphic/pages/donut/donut.component.ts
+++ src/app/graphic/pages/donut/donut.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { GraphicsService } from '../../services/graphics.service';
+// import DatalabelsPlugin from 'chartjs-plugin-datalabels';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-donut',
@@ -11,6 +15,33 @@ export class DonutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  
   }
+
+  // Pie
+  // public pieChartOptions: ChartConfiguration['options'] = {
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       display: true,
+  //       position: 'top',
+  //     },
+  //     datalabels: {
+  //       formatter: (value, ctx) => {
+  //         if (ctx.chart.data.labels) {
+  //           return ctx.chart.data.labels[ctx.dataIndex];
+  //         }
+  //       },
+  //     },
+  //   }
+  // };
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ],
+    datasets: [ {
+      data: [ 300, 500, 100 ]
+    } ]
+  };
+  public pieChartType: ChartType = 'pie';
+  // public pieChartPlugins = [ DatalabelsPlugin ];
 
 }
