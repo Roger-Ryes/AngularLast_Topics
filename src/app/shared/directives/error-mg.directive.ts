051e8e8f76f503aeea85d893ad4217a498173627
Roger-Rys <roger.reyesm97@gmail.com>
2022-05-06 22:28:18 -0500

    parametros en directivas

--- src/app/shared/directives/error-mg.directive.ts
+++ src/app/shared/directives/error-mg.directive.ts
@@ -1,15 +1,29 @@
-import { Directive, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[errorMsg]'
 })
 export class ErrorMgDirective implements OnInit{
 
-  constructor() { 
-    console.log("constructor directive")
-  }
-  ngOnInit(): void {
-    console.log("onnit directive");
+  public htmlElement: ElementRef<HTMLElement>;
+  @Input() color: string = "red";
+  @Input() message: string = "Mensage de error";
+
+  constructor( private el: ElementRef<HTMLElement>) { 
+    this.htmlElement = el;
+    console.log(el)
   }
 
+  ngOnInit(): void {
+    this.setColor();
+    this.setMessage();
+  }
+
+  setColor(): void{
+    this.htmlElement.nativeElement.classList.add("form-text");
+    this.htmlElement.nativeElement.style.color = this.color;
+  }
+  setMessage(): void{
+    this.htmlElement.nativeElement.innerText = this.message;
+  }
 }
